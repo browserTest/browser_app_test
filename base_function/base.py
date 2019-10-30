a8ae74217f1d0be6adf3956dbe40ef329f115b97
luyouxuan <luyouxuan@meizu.com>
2019-10-30 16:02:53 +0800

    完善长按操作方法

--- base_function/base.py
+++ base_function/base.py
@@ -190,18 +190,18 @@ class Base():
 
 
     # 根据元素名称进行长按操作——LYX
-    def long_clickByElement(self, element, logtext):
+    def long_clickByElement(self, element, logtext,duration=5):
         '''
         :param element: 元素名称，可根据resource、坐标及Text进行判断并长按
         :param logtext: 打印log的文案
         :return:
         '''
         if str(element).startswith("com"):
-            self.d(resourceId=element).long_click()
+            self.d(resourceId=element).long_click(duration)
         elif type(element) == tuple:
-            self.d.long_click(element[0],element[1])
+            self.d.long_click(element[0],element[1],duration)
         else:
-            self.d(text=element).long_click()
+            self.d(text=element).long_click(duration)
         logging.info("长按元素: {}".format(logtext))
 
     # 根据元素id及text组合进行长按操作——LYX
@@ -213,3 +213,5 @@ class Base():
         '''
         self.d(resourceId=id, text= text).long_click()
         logging.info("点击元素： {}".format(logtext))
+
+
