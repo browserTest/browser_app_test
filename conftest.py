aef5b0f0459d71cfa07e8b6a74ff8adaa4f06b45
luyouxuan <luyouxuan@meizu.com>
2019-12-03 17:59:04 +0800

    修改默认执行机器

--- conftest.py
+++ conftest.py
@@ -12,9 +12,7 @@ def pytest_addoption(parser):
     :param parser:
     :return:
     """
-    # parser.addoption("--cmdopt",  default="172.18.10.12", help="手机的IP地址")
-
-    parser.addoption("--cmdopt", default="172.29.153.125", help="手机的IP地址")
+    parser.addoption("--cmdopt",  default="172.18.8.112", help="手机的IP地址")
 
 @pytest.fixture
 def cmdopt(request):
